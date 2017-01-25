d008e37f689e64eac326dc0292157eeecacc0395
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-01-25 21:01:26 +0530

    added view pager

--- app/src/main/java/com/rishabhshukla/rubio/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/rubio/MainActivity.java
@@ -1,14 +1,106 @@
 package com.rishabhshukla.rubio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
+    public static final int NUM_PAGES=3;
+    int[] layouts;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //rdfhgdtf
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        layouts = new int[]{
+                R.layout.fragment_slide1,
+                R.layout.fragment_slide2,
+                R.layout.fragment_slide3,
+        };
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setClipToPadding(false);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+       // mPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+
+
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        private LayoutInflater layoutInflater;
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:{
+                    return new Slide1Fragment();
+                }
+                case 1:{
+                    return new Slide2Fragment();
+                }
+                case 2:{
+                    return new Slide3Fragment();
+                }
+                default:{
+                    return new Slide1Fragment();
+                }
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return 1.0f;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
