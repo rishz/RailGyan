4a984d04da5240be9c7de6b7b08140171c596f4c
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-01-26 08:06:15 +0530

    login screen added

--- app/src/main/java/com/rishabhshukla/rubio/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/rubio/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
         layouts = new int[]{
                 R.layout.fragment_slide1,
@@ -86,17 +84,12 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //nonspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
